04ef62c4a39802668fa53a3a4b9b720378e4af71
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-08-28 01:03:25 +0530

    array operations changes

--- array_operations.c
+++ array_operations.c
@@ -2,31 +2,101 @@
 #include<stdlib.h>
 
 int main()
-{
-    int a[100],i,n,search,found=0;
+{  
+    //Inserting element in an array
+
+    int a[100], pos,elem,n,i;
+    char choice;
     printf("Enter the number of elements \n");
-        scanf("%d",&n);
+    scanf("%d",&n);
     printf("Enter the array elements \n");
     for(i=0;i<n;i++){
     scanf("%d",&a[i]);
     }
-    printf("The array element to be searched \n");
-     scanf("%d",&search);
-    for(i=0;i<n;i++)
+    printf("Enter the element to be inserted:");
+    scanf("%d",&elem);
+    printf("Enter the position u want to insert:");
+    scanf("%d",&pos);
+    if(pos<1||pos>n+1)
     {
-        if(a[i]==search)
-        {
-            found=1;
-            break;
-        }
-           if(found==1)
-           {
-            printf("Element %d found in the position %d");
-           }
-        else{
-            printf("Element %d not found!!");
-        }
-        return 0;
+        printf("Invalid position");
+        return 1;
     }
+    for(i=n-1;i>=pos-1;i--){
+        a[i+1]=a[i];
+        
+    }
+    a[pos-1]=elem;
+    n++;
+    printf("The updated array is:");
+         for(i=0;i<n;i++)
+         {
+            printf("%d\t",a[i]);
+             
+         }
+        
+         printf("\nDo you want delete any element (Y/N)?");
+         
+          scanf(" %c",&choice);
+             if(choice=='Y'||choice=='y')
+             {
+                printf("Enter the postion of Element to be deleted ");
+                scanf("%d",&pos);
+                for(i=pos-1;i<n-1;i++)
+                {
+                    a[i]=a[i+1];    //main part
+                }
+                 n--;
+                 
+       printf("The updated array after deleting the element is:\n");
+          for (i = 0; i < n; i++) {
+        printf("%d\t", a[i]);
+           }
+
+             }else if (choice=='N'|| choice=='n')
+             {
+                printf("You choose to exit!");
+             }
+              else{
+                printf("Invalid option");
+              }
+
+             
+             
+        
+    
+     
+    
+
+
+
+
+    // int a[100],i,n,search,found=0;
+    // printf("Enter the number of elements \n");
+    //     scanf("%d",&n);
+    // printf("Enter the array elements \n");
+    // for(i=0;i<n;i++){
+    // scanf("%d",&a[i]);
+    // }
+    // printf("The array element to be searched \n");
+    //  scanf("%d",&search);
+     
+    // for(i=0;i<n;i++)
+    // {
+    //     if(a[i]==search)
+    //     {
+    //         found=1;
+    //         break;
+    //     }
+    // }
+    //        if(found==1)
+    //        {
+    //         printf("Element %d found in the position %d",search,i);
+    //        }
+    //     else{
+    //         printf("Element %d not found!!",search);
+    //     }
+    //     return 0;
+    
       
 }
--- negetive_positive.c
+++ negetive_positive.c
@@ -1,7 +1,55 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-int main()
-{
-   
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int fun(int arr[], int l, int h)
+// {
+//     int min = 0; // Initialize 'min' to a large value
+//     if (l + arr[l] < h)
+//     {
+//         int y = fun(arr, l + arr[l], h);
+//         min = y + 1;
+//     }
+//     return min;
+// }
+// int main()
+// {
+//     int l = 0, h = 10;
+//     int arr[] = {1,3,5,8,-2,9,10,4,7,6};
+//     int c = 0;
+//     c= fun(arr, l, h);
+//     printf("value of min = %d", c);
+//     return 0;
+// }
