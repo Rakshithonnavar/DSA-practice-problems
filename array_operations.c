02999ba023f0b8f6796e9c14b3ab6a7bac0bd5d5
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-08-31 01:16:19 +0530

    array_operations changes

--- array_operations.c
+++ array_operations.c
@@ -1,120 +1,146 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
-int main()
-{  
-    //Inserting element in an array
+//  int main()
+// {  
+//     //Inserting element in an array
 
-    int a[100], pos,elem,n,i,search,found=0;
-    char choice;
-    printf("Enter the number of elements \n");
-    scanf("%d",&n);
-    printf("Enter the array elements \n");
-    for(i=0;i<n;i++){
-    scanf("%d",&a[i]);
-    }
-    printf("Enter the element to be inserted:");
-    scanf("%d",&elem);
-    printf("Enter the position u want to insert:");
-    scanf("%d",&pos);
-    if(pos<1||pos>n+1)
-    {
-        printf("Invalid position");
-        return 1;
-    }
-    for(i=n-1;i>=pos-1;i--){
-        a[i+1]=a[i];
+//     int a[100], pos,elem,n,i,search,found=0;
+//     char choice;
+//     printf("Enter the number of elements \n");
+//     scanf("%d",&n);
+//     printf("Enter the array elements \n");
+//     for(i=0;i<n;i++){
+//     scanf("%d",&a[i]);
+//     }
+//     printf("Enter the element to be inserted:");
+//     scanf("%d",&elem);
+//     printf("Enter the position u want to insert:");
+//     scanf("%d",&pos);
+//     if(pos<1||pos>n+1)
+//     {
+//         printf("Invalid position");
+//         return 1;
+//     }
+//     for(i=n-1;i>=pos-1;i--){
+//         a[i+1]=a[i];
         
-    }
-    a[pos-1]=elem;
-    n++;
-    printf("The updated array is:");
-         for(i=0;i<n;i++)
-         {
-            printf("%d\t",a[i]);
+//     }
+//     a[pos-1]=elem;
+//     n++;
+//     printf("The updated array is:");
+//          for(i=0;i<n;i++)
+//          {
+//             printf("%d\t",a[i]);
              
-         }
+//          }
         
-         printf("\nDo you want delete any element (Y/N)?");
+//          printf("\nDo you want delete any element (Y/N)?");
          
-          scanf(" %c",&choice);
-             if(choice=='Y'||choice=='y')
-             {
-                printf("Enter the postion of Element to be deleted ");
-                scanf("%d",&pos);
-                for(i=pos-1;i<n-1;i++)
-                {
-                    a[i]=a[i+1];    //main part
-                }
-                 n--;
+//           scanf(" %c",&choice);
+//              if(choice=='Y'||choice=='y')
+//              {
+//                 printf("Enter the postion of Element to be deleted ");
+//                 scanf("%d",&pos);
+//                 for(i=pos-1;i<n-1;i++)
+//                 {
+//                     a[i]=a[i+1];    //main part
+//                 }
+//                  n--;
                  
-       printf("The updated array after deleting the element is:\n");
-          for (i = 0; i < n; i++) {
-        printf("%d\t", a[i]);
-           }
+//        printf("The updated array after deleting the element is:\n");
+//           for (i = 0; i < n; i++) {
+//         printf("%d\t", a[i]);
+//            }
 
-             }else if (choice=='N'|| choice=='n')
-             {
-                printf("You choose to exit!");
-             }
-              else{
-                printf("Invalid option");
-              }
-              printf("\nThe array element to be searched ");
-                scanf("%d",&search);
-                for(i=0;i<n;i++)
+//              }else if (choice=='N'|| choice=='n')
+//              {
+//                 printf("You choose to exit!");
+//              }
+//               else{
+//                 printf("Invalid option");
+//               }
+//               printf("\nThe array element to be searched ");
+//                 scanf("%d",&search);
+//                 for(i=0;i<n;i++)
+//     {
+//         if(a[i]==search)
+//         {
+//             found=1;
+//             break;
+//         }
+//     }
+//            if(found==1)
+//            {
+//             printf("Element %d found in the position %d",search,i);
+//            }
+//         else{
+//             printf("Element %d not found!!",search);
+//            }
+//            return 0;
+//     int a[100],i,n,search,found=0;
+//     printf("Enter the number of elements \n");
+//         scanf("%d",&n);
+//     printf("Enter the array elements \n");
+//     for(i=0;i<n;i++){
+//     scanf("%d",&a[i]);
+//     }
+//     printf("The array element to be searched \n");
+//      scanf("%d",&search);
+     
+//     for(i=0;i<n;i++)
+//     {
+//         if(a[i]==search)
+//         {
+//             found=1;
+//             break;
+//         }
+//     }
+//            if(found==1)
+//            {
+//             printf("Element %d found in the position %d",search,i);
+//            }
+//         else{
+//             printf("Element %d not found!!",search);
+//         }
+//         return 0;
+// }
+    
+  //Largest and Smallest element in array
+     
+
+    int getmin(int a[],int n)
     {
-        if(a[i]==search)
-        {
-            found=1;
-            break;
-        }
+        if(n==1)
+            return a[0];
+            int min1= getmin(a,n-1);
+          return(a[n-1]<min1)? a[n-1]:min1;
     }
-           if(found==1)
-           {
-            printf("Element %d found in the position %d",search,i);
-           }
-        else{
-            printf("Element %d not found!!",search);
-           }
-           return 0;
-
-             
-             
+     int getmax(int a[],int n)
+     {
+        if(n==1)
+        return a[0];
+         int max1 = getmax(a,n-1);
+      return(a[n-1]>max1)? a[n-1]:max1;
         
-    
-     
-    
+     }
+
+    int main()
+    {
+        int n,a[100],min,max;
+        printf("Enter size");
+        scanf("%d",&n);
+        printf("Enter array elements");
+        for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+          min = getmin(a,n);
+          max = getmax(a,n);
+       printf("Minimum value in the array: %d\n", min);
+       printf("Maximum value in the array: %d\n", max);
+    }
 
 
 
-
-    // int a[100],i,n,search,found=0;
-    // printf("Enter the number of elements \n");
-    //     scanf("%d",&n);
-    // printf("Enter the array elements \n");
-    // for(i=0;i<n;i++){
-    // scanf("%d",&a[i]);
-    // }
-    // printf("The array element to be searched \n");
-    //  scanf("%d",&search);
-     
-    // for(i=0;i<n;i++)
-    // {
-    //     if(a[i]==search)
-    //     {
-    //         found=1;
-    //         break;
-    //     }
-    // }
-    //        if(found==1)
-    //        {
-    //         printf("Element %d found in the position %d",search,i);
-    //        }
-    //     else{
-    //         printf("Element %d not found!!",search);
-    //     }
-    //     return 0;
-    
       
-}
--- sorting_algos.c
+++ sorting_algos.c
@@ -1,98 +1,86 @@
 #include<stdio.h>
+#include<math.h>
 //#include<stdlib.h>
-void swap(int *a, int *b);
-void Sort(int a[], int n);
-void wave_sort(int a[], int n);
-
-int main()
-{
-    int a[100],n,i;
-    printf("Enter size of array");
-    scanf("%d",&n);
-    printf("Enter the array elements");
-      for(i=0;i<n;i++){
-    scanf("%d",&a[i]);
-      }
-    wave_sort(a,n);
-    printf("The array after performing Wave sort operation is:");
-    for(i=0;i<n;i++){
-    printf("%d\t",&a[i]);
-    }
-}
-void Sort(int a[], int n)
-{
-    int i, j;
-    for (i = 0; i < n - 1; i++)
-        for (j = 0; j < n - i - 1; j++)
-            if (a[j] > a[j + 1])
-                swap(&a[j], &a[j + 1]);
-}
-
-void swap(int *a,int *b)
-{
-     int temp = *a;
-    *a =*b;
-    *b =temp;
-}
-void wave_sort(int a[],int n)
-{
-    Sort(a,n);
-
-    for(int i=0;i<n-1;i+=2)
-    {
-        swap(&a[i], &a[i+1]);
-    }  
-    return 0;
-}
-
-//  #include <stdio.h>
-
 // void swap(int *a, int *b);
 // void Sort(int a[], int n);
 // void wave_sort(int a[], int n);
 
-// int main() {
-//     int a[100], n, i;
-
-//     printf("Enter size of array: ");
-//     scanf("%d", &n);
-
-//     printf("Enter the array elements:\n");
-//     for (i = 0; i < n; i++) {
-//         scanf("%d", &a[i]);
+// int main()
+// {
+//     int a[100],n,i;
+//     printf("Enter size of array");
+//     scanf("%d",&n);
+//     printf("Enter the array elements");
+//       for(i=0;i<n;i++){
+//     scanf("%d",&a[i]);
+//       }
+//     wave_sort(a,n);
+//     printf("The array after performing Wave sort operation is:");
+//        for(i=0;i<n;i++){
+//         printf("%d\t",a[i]);
 //     }
+//     return 0;
+// }
+// void swap(int *a,int *b)
+// {
+//      int temp = *a;
+//     *a =*b;
+//     *b =temp;
+// }
+// void Sort(int a[], int n)
+// {
+//     int i, j;
+//     for (i = 0; i < n - 1; i++)
+//         for (j = 0; j < n - i - 1; j++)
+//             if (a[j] > a[j + 1])
+//                 swap(&a[j], &a[j + 1]);
+// }
 
-//     wave_sort(a, n);
 
-//     printf("The array after performing Wave sort operation is:\n");
-//     for (i = 0; i < n; i++) {
-//         printf("%d\t", a[i]);
-//     }
+// void wave_sort(int a[],int n)
+// {
+//     Sort(a,n);
 
+//     for(int i=0;i<n-1;i+=2)
+//     {
+//         swap(&a[i], &a[i+1]);
+//     }  
 //     return 0;
 // }
 
-// void swap(int *a, int *b) {
-//     int temp = *a;
-//     *a = *b;
-//     *b = temp;
-// }
+      //Insertion Sort
 
-// void Sort(int a[], int n) {
-//     int i, j;
-//     for (i = 0; i < n - 1; i++) {
-//         for (j = 0; j < n - i - 1; j++) {
-//             if (a[j] > a[j + 1]) {
-//                 swap(&a[j], &a[j + 1]);
-//             }
-//         }
-//     }
-// }
+      void insertion_sort(int a[],int n)
+      {
+        int i,j,key;
+        for(i=1;i<n;i++)
+        {
+            key=a[i];
+            j=i-1;
 
-// void wave_sort(int a[], int n) {
-//     Sort(a, n);
+            while(j>=0 &&a[j]>key)
+            {
+                  a[j+1] = a[j];
+                  j=j-1;
+            }
+             a[j+1]=key;
+        }
+      }
 
-//     for (int i = 0; i < n - 1; i += 2) {
-//         swap(&a[i], &a[i + 1]);
-//     }
-// }
+      int main()
+      {
+        int a[100],n,i;
+    printf("Enter size of array");
+    scanf("%d",&n);
+    printf("Enter the array elements");
+      for(i=0;i<n;i++){
+      scanf("%d",&a[i]);
+      }
+      insertion_sort(a,n);
+      printf("The array elements after sorting are \n");
+      for(i=0;i<n;i++){
+      printf("%d\t",a[i]);
+      }
+     return 0;
+     }
+      
