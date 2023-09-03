c7398ced81ee11d34671a969685bded2ee084173
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-09-03 12:21:05 +0530

    subarrays changes

--- 2D_Arrays.c
+++ 2D_Arrays.c
@@ -6,48 +6,48 @@
 # define R 4
 # define C 4
 
-void rotate_matrix(int a[N][N])
-    {
-     int i,j,temp;
-     for(i=0;i<N/2;i++){
-       for(j=i;j<N-i-1;j++){
-        temp = a[i][j];
-         a[i][j] = a[N-1-j][i];
-         a[N-1-j][i] = a[N-1-i][N-1-j];                                                              
-         a[N-1-i][N-1-j] = a[j][N-1-i];
-         a[j][N-1-i] = temp;
-       }  
-}
-}
-int main()
-{
-     int a[N][N] ,i ,j;
-     printf("Enter the Matrix elements");                         //    t=a   a=b   b=c  c =d     d=t
-     for (i=0 ; i < N ; ++i){
-          for(j=0;j<N;j++){
-          scanf("%d",&a[i][j]);  
-     }
-     }
+// void rotate_matrix(int a[N][N])
+//     {
+//      int i,j,temp;
+//      for(i=0;i<N/2;i++){
+//        for(j=i;j<N-i-1;j++){
+//         temp = a[i][j];
+//          a[i][j] = a[N-1-j][i];
+//          a[N-1-j][i] = a[N-1-i][N-1-j];                                                              
+//          a[N-1-i][N-1-j] = a[j][N-1-i];
+//          a[j][N-1-i] = temp;
+//        }  
+// }
+// }
+// int main()
+// {
+//      int a[N][N] ,i ,j;
+//      printf("Enter the Matrix elements");                         //    t=a   a=b   b=c  c =d     d=t
+//      for (i=0 ; i < N ; ++i){
+//           for(j=0;j<N;j++){
+//           scanf("%d",&a[i][j]);  
+//      }
+//      }
 
-      rotate_matrix(a);  
-     printf("Matrix elements after rotating are:\n");  
-    for(i=0;i<N;i++){
-     for(j=0;j<N;j++){
-        printf("%d\t",a[i][j]);
-     }
-        printf("\n");
-    }
+//       rotate_matrix(a);  
+//      printf("Matrix elements after rotating are:\n");  
+//     for(i=0;i<N;i++){
+//      for(j=0;j<N;j++){
+//         printf("%d\t",a[i][j]);
+//      }
+//         printf("\n");
+//     }
         
-     return 0;
-}
+//      return 0;
+// }
 
 
 // Rotate matrix in spiral order
-
   void spiral_print(int a[N][N])
   {
-   
+          
           int i,k=0,l=0,m,n;
+          m=n=N; 
           while(k<m && l<n)
           {
                for(i=1;i<n;i++)
@@ -92,7 +92,7 @@ int main()
      }
      printf("The Matrix elements after performing spiral rotation are:");
       spiral_print(a);
-    return 0;
+       return 0;
   }
 
 
--- subarrays.c
+++ subarrays.c
@@ -2,33 +2,59 @@
 #include <stdlib.h>
 #include<limits.h>
 
-int find_maximum_subarray(int array[], int n, int k) {
-  int i, j, max_sum = INT_MIN;
-  int current_sum = 0;
-
-  for (i = 0; i < n - k + 1; i++) {
-    current_sum = 0;
-    for (j = i; j < i + k; j++) {
-      current_sum += array[j];
-    }
-    if (current_sum > max_sum) {
-      max_sum = current_sum;
-    }
+int  contigious_array(int a[],int n)
+{
+  int max_so_far=INT_MIN,max_ending_here=0;
+  for(int i=0;i<n;i++)
+  {
+     max_ending_here = max_ending_here+a[i];
+          if(max_ending_here>max_so_far)
+           max_so_far = max_ending_here ;
   }
-
-  return max_sum;
+       if(max_ending_here<0)
+           max_ending_here=0;
+    printf("The contigious sum of arrays:",max_so_far);
 }
 
-int main() {
-  int array[] = {1,2,3,1,4,5,2,3,6};
-  int n = sizeof(array) / sizeof(array[0]);
-  int k = 3;
-
-  int max_sum = find_maximum_subarray(array, n, k);
-
-  printf("The maximum subarray sum is %d\n", max_sum);
-
-  return 0;
+int main()
+{
+  int a[100],n,i,max_so_far;
+  printf("Enter the array size:");
+  scanf("%d",&n);
+  printf("Enter the array elements");
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+    int max_sum = contigious_array(a,n);
+        return 0;
 }
 
+// int find_maximum_subarray(int array[], int n, int k) {
+//   int i, j, max_sum = INT_MIN;
+//   int current_sum = 0;
 
+//   for (i = 0; i < n - k + 1; i++) {
+//     current_sum = 0;
+//     for (j = i; j < i + k; j++) {
+//       current_sum += array[j];
+//     }
+//     if (current_sum > max_sum) {
+//       max_sum = current_sum;
+//     }
+//   }
+
+//   return max_sum;
+// }
+
+// int main() {
+//   int array[] = {-2,-3,4,-1,-2,1,5,-3};
+//   int n = sizeof(array) / sizeof(array[0]);
+//   int k = 5;
+
+//   int max_sum = find_maximum_subarray(array, n, k);
+
+//   printf("The maximum subarray sum is %d\n", max_sum);
+
+//   return 0;
+// }
