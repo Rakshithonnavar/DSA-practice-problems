012e1bbadc69ee19f18f02ae27171b359b316605
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-09-06 09:37:31 +0530

    commit

---
+++ strings.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Storing in temporaray variable
+// void reverse_string(char *a,char *z)
+
+// {
+//     char temp;
+//     while(*a<*z){
+//       temp = *a;
+//       *a++ = *z;
+//       *z-- =temp;
+// }
+// }
+
+// void reverse_words(char*s)
+// {
+//   char * word_begin = s;
+//   char * temp=s;
+
+//    //Reversing individual words 
+//      while(*temp){
+//        if(*temp=='\0'){
+//           reverse_string(word_begin,temp-1);
+//        }
+//      else if(*temp ==' ')
+//      {
+//          reverse_string(word_begin,temp-1);
+//          word_begin = temp+1;
+//      }
+//       temp++;
+//      }
+//        reverse_string(s,temp-1);
+// }
+
+// int main()
+// {
+//   char s[100];
+//   // char *temp = s;
+
+//   printf("Enter the sentence:");
+//   scanf("%[^\n]",s);
+//   reverse_words(s);
+//   printf("\nThe reversed string is:%s\n",s);
+//    return 0;
+
+// }
+
+
