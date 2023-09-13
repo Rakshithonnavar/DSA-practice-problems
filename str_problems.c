f4c25829f10d2a49e60e07ee1c05906235b0a708
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-09-14 01:20:24 +0530

    str_probloems.c latest changes

--- str_problems.c
+++ str_problems.c
@@ -2,32 +2,41 @@
 #include<string.h>
 #include<stdio.h>
 
-int divisibleby7(int n)
-{
- if(n<0){
-    return -n;
- }
-else if(n==0||n==7){
-    return 1;
-}
-else if(n<10){
-    return 10;
-}
-else return (n/10-2)*(n-n/10*10);
-}
+// int divisibleby7(int n)
+// {
+//  if(n<0){
+//     return -n;
+//  }
+// else if(n==0||n==7){
+//     return 1;
+// }
+// else if(n<10){
+//     return 10;
+// }
+// else return (n/10-2)*(n-n/10*10);
+// }
 
-int main()
+// int main()
+// {
+//     int n,result;
+//     printf("Enter num");
+//     scanf("%d",&n);
+//     if( divisibleby7(n))
+//     {
+//         printf("the num is divisible");
+//     }
+//     else{
+//         printf("Number is Not divisible");
+//     }
+// }
+
+int isomorphic(char s1[],char s2[],int n)
 {
-    int n,result;
-    printf("Enter num");
-    scanf("%d",&n);
-    if( divisibleby7(n))
-    {
-        printf("the num is divisible");
-    }
-    else{
-        printf("Number  is Not divisible");
-    }
+    int visited;
+   if(strlen(s1==s2))
+   {
+      
+   }
 }
 
 
