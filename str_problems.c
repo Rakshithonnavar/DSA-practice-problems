f4ea0b3a969ce0587bfa374b6ccdbfc520a575f7
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-09-14 20:42:47 +0530

    str_probloems.c latest changes

--- str_problems.c
+++ str_problems.c
@@ -30,15 +30,98 @@
 //     }
 // }
 
-int isomorphic(char s1[],char s2[],int n)
+// int anagram(char s1[],char s2[],int n)
+// {
+//     int visited,k;
+//    if(strlen(s1==s2))
+//    {
+//       char MAX_CHAR;
+//       int count1[MAX_CHAR];
+//       int count2[MAX_CHAR];
+
+//       for(int i=0;i<MAX_CHAR;i++)
+//       {
+//          count1[i]=0;
+//          count2[i]=0;
+//       }
+//       for(int i=0;i<n;i++)
+//         count1[s1[i]-'a']++;
+//       for(int i=0;i<n;i++)
+//          count2[s2[i]-'a']++;
+
+//          int count=0;
+
+//          for(int i=0;i<MAX_CHAR;i++)
+//             if(count1[i] > count2[i])
+//                 count = count + abs(count1[i]-count2[i]);
+
+//                 return(count<=k);
+
+
+//    }
+// }
+
+// int main()
+// {
+//    char s1[100];
+//    char s2[100];
+//    int k,n;
+// printf("Enter the strings:");
+//   scanf("%s%s",s1,s2);
+// printf("Enter the value of k");
+// scanf("%d",&k);
+//   if(anagram(s1,s2,n))
+//   {
+//     return 1;
+//   }
+//   else
+//   {
+//    return 0;
+//   }
+// }
+
+int  panagram(char str[])
 {
-    int visited;
-   if(strlen(s1==s2))
-   {
-      
-   }
+  int  mark[26];
+    for(int i=0;i<26;i++)
+      mark[i] = 0;
+
+      int index;
+
+
+  int len= strlen(str);
+  for(int i=0;i<len;i++)
+  {
+       if(str[i]>='A' && str[i]<='Z')
+         index = str[i]-'A';
+        
+      else if(str[i]>='a'&& str[i]<='z')
+         index = str[i]-'a';
+
+     else
+        continue;
+    mark[index] = 1;
+  }
+
+  for(int i=0;i<=25;i++)
+     if(mark[i]==0)
+        return 0;
+        return 1;
 }
 
+int main()
+{
+char str[100];
+printf("\n Enter a string: ");
+scanf("%s",str);
+if (panagram(str)==1)
+   {
+    printf("its a panagram");
+   } 
+else{
+  printf("not a panagram");
+}
+}
 
 
 
