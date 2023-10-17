92bbcaf83e2d4fce838c8e4c2bbe0eee101e40ce
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-17 20:24:29 +0530

    permutations

---
+++ permutations.c
@@ -0,0 +1,59 @@
+ #include<stdlib.h>
+ #include<stdio.h>
+ #include<string.h>
+
+
+  void swap(char *l,char *r)
+  {
+    char *temp = *l;
+          *l=*r;
+          *r=temp;      
+  }
+ int permute(char *str,int l,int r)
+ {
+    l=0;
+    r= strlen(str)-1;
+    if(l==r)
+       return str;
+    else{
+   for(int i=l;i<=r;i++)
+   {
+     swap((str+l),(str+i));
+     permute(str,l+1,r);
+     swap((str+l),(str+i));
+   }       
+ }
+ }
+
+ int main()
+ {
+    char str[100];
+    int l=0;
+    int r= strlen(str)-1;
+     printf("Enter the string:");
+     scanf("%s",str);
+     printf("\n%s\n",permute(str,0,r));
+     return 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+             
