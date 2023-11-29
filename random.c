0b53fd14e9d0d571f1fee42abe11ced2f2cb90b8
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-11-29 20:42:43 +0530

    latest commit

--- random.c
+++ random.c
@@ -5,43 +5,43 @@ int main()
 {
 
   
-int a[100],i,n,low,high,key,mid,pos=-1;
-printf("Enter the number of elements ");
-scanf("%d",&n);
-printf("Enter the elements ");
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-printf("Enter key elem to be searched ");
-scanf("%d",&key);
-low = 0;
-high = n - 1;
-while(low<=high)
-{
-   mid=(low+high)/2;
+// int a[100],i,n,low,high,key,mid,pos=-1;
+// printf("Enter the number of elements ");
+// scanf("%d",&n);
+// printf("Enter the elements ");
+// for(i=0;i<n;i++)
+// {
+// scanf("%d",&a[i]);
+// }
+// printf("Enter key elem to be searched ");
+// scanf("%d",&key);
+// low = 0;
+// high = n - 1;
+// while(low<=high)
+// {
+//    mid=(low+high)/2;
 
-if(a[mid]==key)
-   {
-    pos=mid;
-    break;
-   }
-else if(a[mid]>key)
-{
-  high=mid-1;
-}
-else{
-  low=mid+1;
-}
-}
-if(pos!=-1)
-{
-   printf("element found! in position %d",pos);
-}
-else{
-  printf("element not found");
-}
-return 0;
+// if(a[mid]==key)
+//    {
+//     pos=mid;
+//     break;
+//    }
+// else if(a[mid]>key)
+// {
+//   high=mid-1;
+// }
+// else{
+//   low=mid+1;
+// }
+// }
+// if(pos!=-1)
+// {
+//    printf("element found! in position %d",pos);
+// }
+// else{
+//   printf("element not found");
+// }
+// return 0;
 // scanf("%d",&elem);
 // scanf("%d",&pos);
 // if(pos<1 ||pos>n+1)
@@ -91,5 +91,24 @@ return 0;
 //   else{
 //     printf("Element not found!");
 //   }
-
+int a[100],n,i,key,found=0;
+printf("Enter no of elems");
+scanf("%d",&n);
+printf("Enter arrya elems");
+ for(i=0;i<n;i++)
+  {
+     scanf("%d",&a[i]);
+  }
+printf("Enter key elem");
+scanf("%d",&key);
+ for(i=0;i<n;i++)
+ {
+    if(a[i]==key){
+      found=1;
+    }
+    break;
+}
+if(found==1)
+{
+  
 }
