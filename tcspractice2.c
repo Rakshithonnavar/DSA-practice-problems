be4a91b6b02ee05184a7825779feaaa365496bee
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-05-05 00:21:22 +0530

    latest commit

--- tcspractice2.c
+++ tcspractice2.c
@@ -69,7 +69,7 @@ void sort_array(int arr[100],int n)
             mid++;
         }
         else{
-            swap(&arr[mid],arr[high]);
+            swap(&arr[mid],&arr[high]);
             high--;
         }
     }
@@ -194,3 +194,51 @@ int main()
   }
   return 0;
 }
+
+//Quick sort:
+int swap(int *a,int *b)
+{
+   int temp = *a;
+    *a=*b;
+    *b= temp;
+}
+void quick_sort(int arr[],int n)
+{
+    int low=0,high=n-1,mid=0;
+    while(mid<=high)
+    {
+        if(arr[mid]==0)
+        {
+            swap(&arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1)
+        {
+           mid++;  
+        }
+        else{
+                swap(&arr[high],&arr[mid]);
+                 high--;
+        }
+    }
+}
+int main()
+{
+    int arr[100],n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter array elements :");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    quick_sort(arr,n);
+    printf("The sorted elements are:");
+        for(int i=0;i<n;i++)
+        {
+            printf("%d ",arr[i]);
+        }
+        return 0;
+}
+//
