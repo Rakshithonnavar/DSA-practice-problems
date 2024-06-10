e82ad15e834e8e6499c8928404c8d23fdd69101b
Rakshithonnavar <rakshithonnavar20@gmail.com>
2024-06-10 23:32:43 +0530

    changes done

--- rotation_algo.c
+++ rotation_algo.c
@@ -41,48 +41,48 @@ int main()
 }
 
 
-//2) Jugling Algorithm
-//   void leftRotate(int a[],int n,int k)
-//     {
-//         int i,j,temp,d,g;
-//           g=gcd(n,k);
-//          for(i=0;i<g;i++)
-//          {
-//             temp=a[i];
-//             j=i;
-//             while(1)
-//             {
-//                 d=j+k;
-//                 if(d>=n)
-//                   d=d-n;
-//                 if(d==i)
-//                    break;
-//               a[j]=a[d];
-//               j=d;
-//             }
-//             a[j]=temp;
-//          }
-//    }
-//       int gcd(int a,int b)
-//           {
-//               if(a%b==0)
-//               return b;
-//               return (b,a%b);   
-//           }
-//     int main()
-// {
-//     int a[100], i,n,k;
-//     printf("Enter the no of elements:");
-//     scanf("%d",&n);
-//     printf("Enter the array elements:");
-//     for(i=0;i<n;i++)
-//     scanf("%d",&a[i]);
-//     printf("Enter no of rotations to be performed:");
-//     scanf("%d",&k);
-//     leftRotate(a,n,k);
-//     for(i=0;i<n;i++)
-//     printf("%d\t",a[i]);
-//     return 0;
+// 2) Jugling Algorithm
+  void leftRotate(int a[],int n,int k)
+    {
+        int i,j,temp,d,g;
+          g=gcd(n,k);
+         for(i=0;i<g;i++)
+         {
+            temp=a[i];
+            j=i;
+            while(1)
+            {
+                d=j+k;
+                if(d>=n)
+                  d=d-n;
+                if(d==i)
+                   break;
+              a[j]=a[d];
+              j=d;
+            }
+            a[j]=temp;
+         }
+   }
+      int gcd(int a,int b)
+          {
+              if(a%b==0)
+              return b;
+              return (b,a%b);   
+          }
+    int main()
+{
+    int a[100], i,n,k;
+    printf("Enter the no of elements:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("Enter no of rotations to be performed:");
+    scanf("%d",&k);
+    leftRotate(a,n,k);
+    for(i=0;i<n;i++)
+    printf("%d\t",a[i]);
+    return 0;
 
 // }
 // 3)rotate array one by one 
