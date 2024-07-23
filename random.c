55a514f39fedbeda9904e4d04a9e18b4903262a6
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-07-23 20:53:24 +0530

    latest commit

--- random.c
+++ random.c
@@ -1933,8 +1933,42 @@ int triFibb(int r)
          n2=n3;
      }
   }
- 
   printf("\n");
+}
+
+void fib(int f[],int N)
+{
+  f[1]=1;
+  f[2]=2;
+
+  for(int i=3;i<=N;i++)
+  {
+    f[i] = f[i-1]+f[i-2];
+  }
+}
+
+void fiboTriangle(int n)
+{
+  int N = n*(n+1)/2;
+  int f[N+1];
+  fib(f,N);
+
+  int fibonum=1;
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=i;j++)
+    {
+        printf("%d\t",f[fibonum++]);   
+    }
+  }
+  printf("\n");
+}
+
+int main()
+{
+  int n=5;
+  fiboTriangle(n);
+  return 0;
 }
 
 
